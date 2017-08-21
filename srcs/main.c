76c7337c127955a96aeff72e0ec5149c323120ec
ywrekt <beerus@MacBook-Pro-de-ywrekt.local>
2017-08-21 05:52:03 +0200

    DD

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: liton <livbrandon@outlook.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/04 19:56:19 by liton             #+#    #+#             */
-/*   Updated: 2017/08/21 05:15:40 by liton            ###   ########.fr       */
+/*   Updated: 2017/08/21 05:51:54 by liton            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char		**strcpy_env(char **envp)
 	return (env);
 }
 
-void			exec_command(char **env, char *cmd)
+void			exec_command(char ***env, char *cmd)
 {
 	char		**av;
 	int			i;
@@ -36,22 +36,26 @@ void			exec_command(char **env, char *cmd)
 	char		**env_path;
 	char		*path;
 
+	
+	if ((i = search_v(*env, "PATH")) == -1)
+	{
+		*env = add_v(*env, "PATH", "/Users/beerus/.rbenv/plugins/ruby-build/bin:/Users/beerus/.rbenv/shims:/Users/beerus/.rbenv/bin:/Users/beerus/.rvm/gems/ruby-2.4.1/bin:/Users/beerus/.rvm/gems/ruby-2.4.1@global/bin:/Users/beerus/.rvm/rubies/ruby-2.4.1/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/beerus/.rvm/bin");	
+		i = size_env(*env) - 1;
+	}
 	pid = fork();
-	if ((i = search_v(env, "PATH")) == -1)
-		return ;
 	if (pid > 0)
 		wait(NULL);
 	else
 	{
 		path = ft_strdup("/");
 		av = ft_strsplit(cmd, ' ');
-		env_path = ft_strsplit(env[i] + 5, ':');
+		env_path = ft_strsplit((*env)[i] + 5, ':');
 		i = 0;
 		while (env_path[i])
 		{
 			path = ft_strjoinfree(env_path[i], path, 1);
 			path = ft_strjoinfree(path, av[0], 1);
-			execve(path, av, env);
+			execve(path, av, *env);
 			free(path);
 			path = ft_strdup("/");
 			++i;
@@ -82,6 +86,6 @@ int				main(int ac, char **av, char **envp)
 			free(builtins);
 		}
 		else
-			exec_command(env, cmd);
+			exec_command(&env, cmd);
 	}
 }
